0be46ddb1488f65e91fa10728b42566d8d0796c8
Ben Stephens <ben_j_stephens@hotmail.com>
2020-06-04 13:55:11 +1000

    seperating javascript

--- start.js
+++ start.js
@@ -3,31 +3,50 @@ var olEl = document.getElementById("ol-scores");
 var submitBtn = document.getElementById("submit");
 var clearBtn = document.getElementById("clear");
 
-var score = Math.floor((Math.random()*10));
+var score = 30;
 
 var saveHighScore = function() {
     console.log("i am saving highscores");
 
-}
-console.log(score)
-// used to check for secondary input method of enter when user presses enter key, run saveHighScore 
-var checkForEnter = function(event) {
-    var intitals = initialsEl.nodeValue.trim();
-    
-    // check if enter is pressed
-    if (event.key === "Enter") {
-        saveHighScore();
-    
+  
+
+    var initials = initialsEl.value.trim();
+
+    if (initials !== "") {
+        
+        // get saved score from local storage, or if not set to empty array
+        var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];     
+
 
         var newScore = {
             score: score,
-            intitals: initials
+            initials: initials  
         };
+
+        //save to local storage
+        highscores.push(newScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+        //used while testing - for clearing the text from the input box
+        // initialsEl.value = ""
     }
 }
+
+// used to check for secondary input method of enter when user presses enter key, run saveHighScore 
+var checkForEnter = function(event) {
+    // check if enter is pressed
+    if (event.key === "Enter") {
+     saveHighScore();
+    }
+ }
+
 
 //if user clicks submit run function
 submitBtn.addEventListener("click", saveHighScore);
 
 // onkeyup to only run once the key is released
-initialsEl.addEventListener("onkeyup", checkForEnter);
+initialsEl.onkeyup = checkForEnter;
+
+
+
+
